f9d28027d8e132114fffe6faced2aa0691852e1e
Mark Rasavong <rasavong.mark@gmail.com>
2023-01-28 17:46:05 +0100

    add: Interpolation binding syntax

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent {
-
+  //interpolation binding syntax
+  hotelName = 'Hilton Hotel';
 }
