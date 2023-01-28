f65630eceef37967b569ecd4b55ce634c76ae174
Mark Rasavong <rasavong.mark@gmail.com>
2023-01-28 17:31:06 +0100

    add: Angular Installation and Binding Syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+//Where all views are loaded
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  //app-root tag @ index.html
+  selector: 'hinv-root',
+  template: `<h1>Angular is awesome!</h1>`,
+  //templateUrl: './app.component.html'
+  styleUrls: ['./app.component.scss'],
+  //styles: [`h1 { color: red; }`],
 })
 export class AppComponent {
   title = 'hotelInventoryApp';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// ANGULAR EVERYTHING is a class. At minium one module to run the application
 
+/* 
+directives, pipes goes to declarations array
+
+*/
+
+//NgModule decorator: mods the class => gives metadata to class
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  //which component will be rendered first
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
