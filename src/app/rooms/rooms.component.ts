93dc1f8308ff8a08ccbfecf14ede7ee89f3a3103
Mark Rasavong <rasavong.mark@gmail.com>
2023-02-02 19:19:33 +0100

    Add: using a service

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { Room, RoomList } from './rooms';
+import { RoomsService } from './services/rooms.service';
 
 @Component({
   selector: 'hinv-rooms',
@@ -18,7 +19,11 @@ import { Room, RoomList } from './rooms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoomsComponent implements OnInit, AfterViewInit, AfterViewChecked {
-  constructor() {}
+  //DI is a design pattern which can be institated throughout the app
+  // best practice to make services private
+  // the component should not get data or any api calls, etx => Always use services
+  constructor(private roomsService: RoomsService) {}
+
   //interpolation binding syntax
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
@@ -72,43 +77,9 @@ export class RoomsComponent implements OnInit, AfterViewInit, AfterViewChecked {
       checkinTime: new Date('11-Nov-2023'),
       checkoutTime: new Date('12-Nov-2021'),
     };
-
-    // IMMUNITABILITY ISSUES: this.roomList.push(room) - this modify data;
-    this.roomList = [...this.roomList, room]; //gives a new instance
   }
 
   ngOnInit(): void {
-    this.roomList = [
-      {
-        roomNumber: 1,
-        roomType: 'Deluxe Room',
-        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 500,
-        photos:
-          'https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-        checkinTime: new Date('11-Nov-2023'),
-        checkoutTime: new Date('12-Nov-2023'),
-      },
-      {
-        roomNumber: 2,
-        roomType: 'Deluxe Room',
-        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 1000,
-        photos:
-          'https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-        checkinTime: new Date('11-Nov-2023'),
-        checkoutTime: new Date('12-Nov-2023'),
-      },
-      {
-        roomNumber: 3,
-        roomType: 'Private Suite',
-        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-        price: 15000,
-        photos:
-          'https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-        checkinTime: new Date('11-Nov-2023'),
-        checkoutTime: new Date('12-Nov-2023'),
-      },
-    ];
+    this.roomList = this.roomsService.getRooms();
   }
 }
--- src/app/rooms/services/rooms.service.ts
+++ src/app/rooms/services/rooms.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { RoomList } from '../rooms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoomsService {
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos:
+        'https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date('12-Nov-2023'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date('12-Nov-2023'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 15000,
+      photos:
+        'https://images.unsplash.com/photo-1629140727571-9b5c6f6267b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+      checkinTime: new Date('11-Nov-2023'),
+      checkoutTime: new Date('12-Nov-2023'),
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getRooms() {
+    return this.roomList;
+  }
 }
